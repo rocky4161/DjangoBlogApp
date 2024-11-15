1f81102500a3896d6f0b5f0e5bf6f3c940742d17
rocky4161 <rocky4161@gmail.com>
2024-11-15 12:05:23 +0530

    First Django Project

---
+++ BlogApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+class EmailSendForm(forms.Form):
+    name=forms.CharField()
+    email=forms.EmailField()
+    to=forms.EmailField()
+    comments=forms.CharField(required=False,widget=forms.Textarea)
+
+from django import forms
+from BlogApp.models import Comment
+class CommentForm(forms.ModelForm):
+	class Meta:
+ 		model=Comment
+ 		fields=('name','email','body')
